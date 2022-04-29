3b188bd2e5fa48a53bb07d241c1ad6cfe96cdbfc
Lynxtail <l.gorbachev@mail.ru>
2022-04-29 17:01:40 +0400

    fix refact

--- Airport.py
+++ Airport.py
@@ -5,7 +5,7 @@ import random
 
 # класс, описывающий сеть
 class Airport:
-    def __init__(self, env, cnt_runways, cnt_servers, mu_runway, 
+    def __init__(self, env:simpy.Environment, cnt_runways, cnt_servers, mu_runway, 
     mu_server, p_out=3):
         self.env = env
         self.runway = simpy.Resource(env, cnt_runways)
@@ -17,18 +17,19 @@ class Airport:
     # обслуживание на взлётке
     def service_runway(self, demand:Demand):
         # yield self.env.timeout(rd.expovariate(self.mu))
-        yield self.env.timeout(env.now - log(random.random()) / 
+        yield self.env.timeout(self.env.now - log(random.random()) / 
             self.mu_runway)
-        global cnt_demands
-        cnt_demands[0].append(self.server.count - 1)
+        # global cnt_demands
+        # cnt_demands[0].append(self.server.count - 1)
         
         print(f'Самолёт {demand.num} прошёл взлётку в {self.env.now}')
            
     # обслуживание на стоянке
     def service_server(self, demand:Demand):
         # yield self.env.timeout(rd.expovariate(self.mu))
-        yield self.env.timeout(env.now - log(random.random()) / 
+        yield self.env.timeout(self.env.now - log(random.random()) / 
             self.mu_server)
-        global cnt_demands
-        cnt_demands[1].append(self.server.count - 1)
-        print(f'Самолёт {demand.num} прошёл обслуживание в {self.env.now}')
+        
+        # global cnt_demands
+        # cnt_demands[1].append(self.server.count - 1)
+        # print(f'Самолёт {demand.num} прошёл обслуживание в {self.env.now}')
